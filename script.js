995b93e4f565081567786d96de22d04c2850cfcd
Urrix <mauribarrien@gmail.com>
2024-02-01 15:49:37 -0600

    end edit

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "YAY! Te Veo el 14 peshiosha";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/IMG_4818.JPEG";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
